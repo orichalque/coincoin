71808e4c373af456ba2fd8a152d50270a6c1a29d
Thibault BEZIERS-LA-FOSSE <E104607D@S046V7pc11.ensinfo.sciences.univ-nantes.prive>
2016-09-26 13:39:01 +0200

    Tuto rmi effectué

---
+++ rmi_test/commons/src/main/java/commons/CommonVariables.java
@@ -0,0 +1,12 @@
+package commons;
+
+/**
+ * Created by E104607D on 26/09/16.
+ */
+public class CommonVariables {
+    private CommonVariables() {
+
+    }
+
+    public static final int PORT = 1152;
+}

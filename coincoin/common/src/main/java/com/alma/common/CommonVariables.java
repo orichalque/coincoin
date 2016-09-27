29ce38074ddb31702240fe358fa7c7d7a3421fd7
Thibault BEZIERS-LA-FOSSE <E104607D@S046V7pc11.ensinfo.sciences.univ-nantes.prive>
2016-09-27 13:40:16 +0200

    Thibault ajout de methodes communes et d item dataTransfertObject

---
+++ coincoin/common/src/main/java/com/alma/common/CommonVariables.java
@@ -0,0 +1,12 @@
+package com.alma.common;
+
+/**
+ * Created by Thibault on 27/09/16.
+ * Define common variables used by both Client and Server
+ */
+public class CommonVariables {
+    private CommonVariables() { }
+
+    public final static String DATA_BASE_NAME = "itemDataBase.json";
+
+}
---
+++ coincoin/common/src/main/java/com/alma/utils/JsonUtil.java
@@ -0,0 +1,7 @@
+package com.alma.utils;
+
+/**
+ * Created by E104607D on 27/09/16.
+ */
+public class JsonUtil {
+}

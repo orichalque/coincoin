0080455b68ba457a17076501327733d090d14495
dennisman <dennisman@laposte.net>
2016-10-06 15:01:31 +0200

    Dennis: Chrono threadé comme thibault l'a demandé

--- coincoin/client/src/main/java/modele/Chrono.java
+++ coincoin/client/src/main/java/modele/Chrono.java
@@ -1,8 +1,29 @@
 package modele;
 
+import business.Client;
+
+import java.rmi.RemoteException;
+
 /**
  * Created by Dennis on 27/09/16.
  */
-public class Chrono {
-    public void run(){}
+public class Chrono extends Thread {
+    private Client client;
+
+    public Chrono(Client client) {
+        this.client = client;
+    }
+
+    public void run() {
+        try {
+            sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        try {
+            client.temps_ecoule();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
 }

0a4e2947682d760c4fe0d545fec5897ac4aa5477
benjimor <benji_moreau@hotmail.fr>
2016-10-06 11:03:01 +0200

    benjamin: client.js GET&POST to update

--- coincoin/client/src/main/webapp/lib/js/client.js
+++ coincoin/client/src/main/webapp/lib/js/client.js
@@ -7,14 +7,14 @@ var coincoinApp = angular.module('coincoinApp', []);
 /**
  * Angular controller linked to <body> tag of index.html
  */
-coincoinApp.controller('itemController', function itemController($scope, $http) {
+coincoinApp.controller('itemController', function itemController($scope, $http, $interval) {
 
     /**
      *
      * Google custom search request
      * Used to find the first occurence of an img search
      */
-    $scope.keyapi ="AIzaSyChJwwtkZUbukQ8Mk-Q6uI6GigZ7t2-T5s"
+    $scope.keyapi =""//"AIzaSyChJwwtkZUbukQ8Mk-Q6uI6GigZ7t2-T5s"
     $scope.cx = "000337515704215858772:qphvdedtcsw"
     $scope.item =
     {
@@ -24,7 +24,7 @@ coincoinApp.controller('itemController', function itemController($scope, $http) 
     };
     $scope.prixActuel = $scope.item.prix;
     $scope.prixPropose = $scope.prixActuel + 1;
-    //$scope.requestUrl = "https://www.googleapis.com/customsearch/v1?key="+$scope.keyapi+"&cx="+$scope.cx+"&q="+$scope.item.nom+"&searchType=image&num=1"
+    $scope.requestUrl = "https://www.googleapis.com/customsearch/v1?key="+$scope.keyapi+"&cx="+$scope.cx+"&q="+$scope.item.nom+"&searchType=image&num=1"
     $http({
         method : "GET",
         url : $scope.requestUrl
@@ -40,8 +40,36 @@ coincoinApp.controller('itemController', function itemController($scope, $http) 
      */
     $scope.changerPrix = function() {
         if ($scope.prixPropose > $scope.prixActuel) {
-            $scope.prixActuel = $scope.prixPropose;
+            var parameters = {nom:$scope.item.nom,newPrice:$scope.prixPropose}
+            $http({
+                method: "POST",
+                url: "/update",
+                params: parameters
+
+            }).then(function mySucces(response) {
+                $scope.prixActuel = $scope.prixPropose;
+            }, function myError(response) {
+                console.log(response.data);
+            });
         }
     };
+
+    /**
+     * This function will be called every 2sec
+     * Used to refresh the UI
+     */
+    $interval(updateInterface, 2000);
+
+    function updateInterface() {
+        $http({
+            method : "GET",
+            url : "/update"
+        }).then(function mySucces(response) {
+            $scope.data = response.data;
+            console.log(response.data);
+        }, function myError(response) {
+            console.log(response.data);
+        });
+    }
 
 });
